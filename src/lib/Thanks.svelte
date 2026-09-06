a98171ba9225548d16ed0045a50af0f47f06c2b4
Kjell Dewald <kjell-dewald@t-online.de>
2026-09-06 12:24:37 +0200

    added umfragelink

--- src/lib/Thanks.svelte
+++ src/lib/Thanks.svelte
@@ -2,5 +2,5 @@
     <p id="welcomeParagraph">Das war es dann mit den Videos! </p>
     <p id="thanksParagraph">Vielen Dank, dass du dir die Zeit genommen hast! Über den unten stehenden Link kommst du zu unserer Umfrage.</p>
     <p id="thanksParagraph">Dabei wirst du einmal gefragt, welchen Coach du am Besten fandest, bevor du noch einen 10 Item Persönlichkeitstest machen kannst. Die Befragung ist dabei komplett anonym und wir erheben keine persönlichen Informationen. Du kannst jederzeit die Befragung abbrechen. </p>
-    <a href="">ZUR UMFRAGE</a>
+    <a href="https://forms.gle/UyudcjtyW27Gg8969">ZUR UMFRAGE</a>
 </section>
